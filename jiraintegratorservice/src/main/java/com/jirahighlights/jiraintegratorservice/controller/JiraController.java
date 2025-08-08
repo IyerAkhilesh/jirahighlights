210122a90d779b23d3cbcda5fd267ca42417cd11
Akhilesh <iyerakhileshk@gmail.com>
2025-08-08 04:02:47 -0400

    removed @values annotation and introuced api properties Java file instead

--- jiraintegratorservice/src/main/java/com/jirahighlights/jiraintegratorservice/controller/JiraController.java
+++ jiraintegratorservice/src/main/java/com/jirahighlights/jiraintegratorservice/controller/JiraController.java
@@ -1,7 +1,6 @@
 package com.jirahighlights.jiraintegratorservice.controller;
 
 import com.jirahighlights.jiraintegratorservice.model.JiraTicket;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
